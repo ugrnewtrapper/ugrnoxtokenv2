cd140e2a8c2e6a2c38e621a63c693fd100f479ca
ugrnewtrapper <134284573+ugrnewtrapper@users.noreply.github.com>
2026-01-15 15:11:40 -0300

    Update calculator.js

--- V33/calculator.js
+++ V33/calculator.js
@@ -1,1 +1,9 @@
+export async function runCalculator(apiKey, match) {
+    console.warn('runCalculator ainda não implementado');
 
+    return {
+        players: 'Aguardando implementação',
+        corners: 'Aguardando implementação',
+        cards: 'Aguardando implementação'
+    };
+}
